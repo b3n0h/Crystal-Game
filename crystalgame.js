07a48ae9c0998d4517a2780e3a913b2bb5707b57
benohhh <b3njaminoh@gmail.com>
2018-08-22 18:38:17 -0700

    created divs to hold goal and total

--- crystalgame.js
+++ crystalgame.js
@@ -1,7 +1,12 @@
 for (let i = 1; i < 4; i++) {
-  $('.crystalRow').append(`
+  $('.rupeeRow').append(`
       <div class="col-md-4">
-      <img class="crystal" id="crystal${i}" data-crystal=${Math.floor(Math.random() * 20) + 1} src="./assets/images/crystal${i}.png" alt="crystal${i}">
+      <img class="rupee" id="rupee${i}" data-rupee=${Math.floor(Math.random() * 20) + 1} src="./images/rupee${i}.jpg" alt="rupee${i}">
       </div>
   `)
 }
+
+// make each one clickable
+$('.rupee').on('click', function () {
+  
+})
